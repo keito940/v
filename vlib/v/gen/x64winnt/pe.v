64bd7496110cced27e94b4a2ae7483cf5c8d0787
Keito Nagai <keito940@live.jp>
2020-04-02 19:01:08 +0900

    create coff format.

--- vlib/v/gen/x64winnt/pe.v
+++ vlib/v/gen/x64winnt/pe.v
@@ -1,2 +1,29 @@
 module x64winnt
 
+const (
+	pe_mag0 = 'P'
+	pe_mag1 = 'E'
+	pe_mag2 = '0'
+	pe_mag3 = '0'
+	dos_mag0 = 'M'
+	dos_mag1 = 'Z'
+)
+
+struct PEHeader {
+	machine u16
+	number_of_sections int
+	time_date_stamp u32
+}
+
+struct DOSHeader{
+
+}
+
+struct BigObjectHeader{
+
+}
+
+const (
+	machine_invalid = 0xffff
+	machine_type = 0x8664 
+)
