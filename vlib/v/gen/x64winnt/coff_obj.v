a9e342b89cb423cf235e53c136c26d69426f6238
Keito Nagai <keito940@live.jp>
2020-04-05 19:47:06 +0900

    IMAGE_FILE_HEADER = ImageFileHeader

--- vlib/v/gen/x64winnt/coff_obj.v
+++ vlib/v/gen/x64winnt/coff_obj.v
@@ -1,2 +1,13 @@
 module x64winnt
 
+#flag -lws2_32
+
+struct ImageFileHeader {
+	Machine u16
+	NumberOfSections u16
+	TimeDateStamp u32
+	PointerToSymbolTable u32
+	NumberOfSymbols u32
+	SizeOfOptionalHeader u16
+	Characterristics u16
+}
