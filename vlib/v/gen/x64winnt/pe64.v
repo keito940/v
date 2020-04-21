fd8d8fb5e3a6d56f3a3b1a30da57c91f3a20bcc8
Keito Nagai <keito940@live.jp>
2020-04-21 19:23:09 +0900

    Reading LLVM COFF Code.

--- vlib/v/gen/x64winnt/pe64.v
+++ vlib/v/gen/x64winnt/pe64.v
@@ -1,21 +1,35 @@
 module x64winnt
-
 const (
 	pe_mag0 = `P`
 	pe_mag1 = `E`
 	pe_mag2 = 0x00
 	pe_mag3 = 0x00
-	dos_mag0 = `M`
-	dos_mag1 = `Z`
 	win_gui_sub_system = 2
 	win_cui_sub_system = 3
 	pe_addr = 0x004
+	big_obj_magic = 
 )
 
-pub fn (g mut Gen) generate_pe64_header {
+struct Symbol {
+	name []byteptr
+	value u32
+	section_number int
+	typ u16
+	storage_class u8
+	number_of_aux_symbols u8
+}
+
+enum SymbolStorageClass{
+	invalid = 0xff
+	end_of_function = -1
+	null = 0
+	automatic = 1
+}
+
+pub fn (mut g Gen) generate_pe64_header {
 
 }
 
-pub fn (g mut Gen) generate_pe64_footer {
+pub fn (mut g Gen) generate_pe64_footer {
 
 }
